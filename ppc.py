0c623d22a9acfbfce97954577f1665c9c0195264
idealtitude <idealtitude@gmail.com>
2016-07-12 07:11:29 +0200

    Added some files, just the very begining... :)

---
+++ ppc.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from os import path
+
+WAI = path.abspath(path.dirname(__file__))
+
+
+def main():
+	print(WAI)
+
+if __name__ == '__main__':
+	main()
+
