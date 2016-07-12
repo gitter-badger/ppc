9ac1095a6f7ef424659d94a6bbf1881e5b264344
idealtitude <idealtitude@gmail.com>
2016-07-12 10:06:05 +0200

    Commencé à coder les éléments de bases

---
+++ core/editor.py
@@ -0,0 +1,85 @@
+import json
+
+import  wx
+import  wx.stc  as  stc
+
+from os import path
+
+WAI = path.abspath(path.dirname(__file__))
+print '1' + WAI
+
+
+class Editor(stc.StyledTextCtrl):
+    """Editeurs
+    """
+    def __init__(self, parent, ID, pos=wx.DefaultPosition, size=wx.DefaultSize, style=0):
+        stc.StyledTextCtrl.__init__(self, parent, ID, pos, size, style)
+
+        self.prnt = parent
+
+        #Editor
+        self.SetEdgeMode(stc.STC_EDGE_BACKGROUND)
+        self.SetEdgeColumn(78)
+        self.SetWrapMode(1)
+        self.SetWrapVisualFlags(1)
+
+        #ZOOM & autres
+        self.CmdKeyAssign(ord('B'), stc.STC_SCMOD_CTRL, stc.STC_CMD_ZOOMIN)
+        self.CmdKeyAssign(ord('N'), stc.STC_SCMOD_CTRL, stc.STC_CMD_ZOOMOUT)
+        self.CmdKeyAssign(ord('W'), stc.STC_SCMOD_CTRL, stc.STC_CMD_WORDRIGHT)
+        self.CmdKeyAssign(ord('W'), stc.STC_SCMOD_CTRL | stc.STC_SCMOD_SHIFT, stc.STC_CMD_WORDLEFT)
+
+        #FOLDING & Margin
+        #self.SetKeyWords(4, " ".join(PHPKEYWORDS))
+        self.SetMarginType(1, wx.stc.STC_MARGIN_NUMBER)
+        self.SetMarginMask(2, stc.STC_MASK_FOLDERS)
+        self.SetProperty("fold", "1")
+        self.SetProperty("fold.html", "1")
+        self.SetMargins(2,2)
+        self.SetMarginSensitive(2, True)
+        self.SetMarginWidth(1, 15)
+        self.SetMarginWidth(2, 12)
+
+        self.MarkerDefine(stc.STC_MARKNUM_FOLDEROPEN,   stc.STC_MARK_BOXMINUS,        "white", "#808080")
+        self.MarkerDefine(stc.STC_MARKNUM_FOLDER,       stc.STC_MARK_BOXPLUS,          "white", "#808080")
+        self.MarkerDefine(stc.STC_MARKNUM_FOLDERSUB,     stc.STC_MARK_VLINE,             "white", "#808080")
+        self.MarkerDefine(stc.STC_MARKNUM_FOLDERTAIL,   stc.STC_MARK_LCORNER,          "white", "#808080")
+        self.MarkerDefine(stc.STC_MARKNUM_FOLDEREND,     stc.STC_MARK_BOXPLUSCONNECTED,  "white", "#808080")
+        self.MarkerDefine(stc.STC_MARKNUM_FOLDEROPENMID, stc.STC_MARK_BOXMINUSCONNECTED, "white", "#808080")
+        self.MarkerDefine(stc.STC_MARKNUM_FOLDERMIDTAIL, stc.STC_MARK_TCORNER,         "white", "#808080")
+
+        #INDENTATION
+        self.SetIndent(4)
+        self.SetIndentationGuides(True)
+        self.SetBackSpaceUnIndents(True)
+        self.SetTabIndents(True)
+        self.SetTabWidth(4)
+        self.SetUseTabs(True)
+        self.SetHighlightGuide(0)
+
+        #HIGHLIGHT & others
+        self.SetStyleBits(7)
+        self.SetViewWhiteSpace(False)
+        self.SetCaretWidth(1)
+        #self.SetControlCharSymbol(0)
+        self.SetCaretLineVisible(True)
+        self.SetCaretLineBack(wx.Colour(250, 254, 255))
+        self.SetCaretForeground("#1A1A1A")
+
+        self.SetLexer(stc.STC_LEX_HTML)
+
+        datas_path = '../datas'
+        php_lex = datas_path + '/lex_php.json'
+        #php_funx = datas_path + '/funx_php.json'
+
+        fo = open(php_lex, 'r')
+        fr = fo.read()
+        fo.close()
+        datas = json.loads(fr)
+
+        for i in datas['styles']:
+            self.StyleSetSpec(eval(i['id']), str(i['val']))
+
+        for i in datas['keywords']:
+            self.SetKeyWords(int(i['index']), str(i['value']))
+
---
+++ layout.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import wx
+import wx.aui
+from core import editor
+
+from os import path
+
+WAI = path.abspath(path.dirname(__file__))
+print '0' + WAI
+
+
+class PhPyCli(wx.Frame):
+    def __init__(self, *args, **kwds):
+        kwds["style"] = wx.DEFAULT_FRAME_STYLE
+        wx.Frame.__init__(self, *args, **kwds)
+        self.statusbar = self.CreateStatusBar(2, 0)
+
+        self.__set_properties()
+
+        main_sizer = wx.BoxSizer(wx.VERTICAL)
+        
+        top_sizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.nb = wx.aui.AuiNotebook(self)
+        #page = wx.TextCtrl(self.nb, -1, '<?php\n\n?>\n', style=wx.TE_MULTILINE)
+        page = editor.Editor(self, -1)
+        page.SetText("<?php\n\n?>\n")
+        self.nb.AddPage(page, "sans titre")
+        
+        rightctl = wx.TextCtrl(self, -1, "", style=wx.TE_MULTILINE)
+        
+        top_sizer.Add(self.nb, 2, wx.EXPAND)
+        top_sizer.Add(rightctl, 1, wx.EXPAND)
+        
+        bottom_sizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        txtctl = wx.TextCtrl(self, -1, "", style=wx.TE_MULTILINE)
+        bottom_sizer.Add(txtctl, 1, wx.EXPAND)
+
+        main_sizer.Add(top_sizer, 3, wx.EXPAND)
+        main_sizer.Add(bottom_sizer, 1, wx.EXPAND)
+        
+        self.SetSizer(main_sizer)
+        self.Layout()
+
+    def __set_properties(self):
+        self.SetTitle("PhPyCli")
+        _icon = wx.EmptyIcon()
+        _icon.CopyFromBitmap(wx.Bitmap("/home/stef/Dev/Python/ppc/icons/ppc-32x32.png", wx.BITMAP_TYPE_ANY))
+        self.SetIcon(_icon)
+        self.SetSize((1024, 780))
+        self.statusbar.SetStatusWidths([-1, -1])
+        statusbar_fields = ["infos", "timer"]
+        for i in range(len(statusbar_fields)):
+            self.statusbar.SetStatusText(statusbar_fields[i], i)
+            
+
+def main():
+    app = wx.App(False)
+    main_window = PhPyCli(None, -1, "")
+    app.SetTopWindow(main_window)
+    main_window.Show()
+    app.MainLoop()
+    
+if __name__ == '__main__':
+	main()
+    
